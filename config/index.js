933273b30857f935b513b73fcc12085bd54f3ade
znareak <libardojesusrengifo129@gmail.com>
2023-05-30 17:12:17 +0200

    cors fix

--- config/index.js
+++ config/index.js
@@ -6,7 +6,12 @@ const SERVER = {
   DEV: process.env.DEV || false,
   API: {
     IS_PRODUCTION: process.env.NODE_ENV === "production",
-    ALLOWED_DOMAINS: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    ALLOWED_DOMAINS: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://infopruebas.vercel.app/",
+      "https://infopruebas.vercel.app",
+    ],
     RATE_LIMITS: {
       windowMs: 10 * 60 * 1000, // 10 minutes
       max: 200, // limit each IP to 200 requests per windowMs
